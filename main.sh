ff28ce41dbaf4816f62f7c7801bd6a79863fa567
wujun8 <winston8.2013@outlook.com>
2021-01-06 17:33:55 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@ today=$(date +"%F")
 if [[ "$today" != $(< run_today) ]] ; then
     random=$(awk 'BEGIN { srand(); print rand() * 100 }')
     random=${random%.*}
-    if [ $random -gt 90 ] ; then
+    if [ $random -gt 79 ] ; then
         echo run
         curl -X POST -H "Authorization: $PAT" -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/wujun8/cccat/actions/workflows/main.yml/dispatches -d '{"ref":"main"}'
         echo $today > run_today
@@ -11,6 +11,6 @@ if [[ "$today" != $(< run_today) ]] ; then
         echo random=$random
     fi
 else
-    echo already run today
+    echo already run today @$today
     exit
 fi
