e7b475e07d1736e9ea34ab74e9289ea6faec8108
wujun8 <winston8.2013@outlook.com>
2021-01-08 16:17:07 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-curl -X POST -H "Authorization: $PAT" -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/wujun8/cccat/actions/workflows/main.yml/dispatches -d '{"ref":"main"}'
+curl -X POST -H "Authorization: token $PAT" -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/wujun8/cccat/actions/workflows/main.yml/dispatches -d '{"ref":"main"}'
